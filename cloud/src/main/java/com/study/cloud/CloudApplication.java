0c39e924736ff8b28c709989a71290bdc38271fb
Malamut73 <lemoor@mail.ru>
2022-09-30 15:44:44 +0400

    Initial commit

---
+++ cloud/src/main/java/com/study/cloud/CloudApplication.java
@@ -0,0 +1,17 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+
+        // http://localhost:8888/test-client.properties
+        // http://localhost:8888/news-client.properties
+        // http://localhost:8888/users-client.properties
+    }
+}
