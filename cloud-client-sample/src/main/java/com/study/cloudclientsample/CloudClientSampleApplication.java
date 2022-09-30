e94160a0f41ab58ec69903290523d08ba1b3b038
Malamut73 <lemoor@mail.ru>
2022-09-30 21:35:00 +0400

    add CloudClientSampleApplication controller and MessageService

---
+++ cloud-client-sample/src/main/java/com/study/cloudclientsample/CloudClientSampleApplication.java
@@ -0,0 +1,15 @@
+package com.study.cloudclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClientSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientSampleApplication.class, args);
+    }
+    // http://localhost:8080/message1
+    // http://localhost:8080/message2
+
+}
---
+++ cloud-client-sample/src/test/java/com/study/cloudclientsample/CloudClientSampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.study.cloudclientsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CloudClientSampleApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
