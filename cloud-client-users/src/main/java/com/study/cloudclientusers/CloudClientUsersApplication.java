8e62e4c17fbecece5680a70cafb1e418c074313d
Malamut73 <lemoor@mail.ru>
2022-10-04 16:11:34 +0400

    add cloud router

---
+++ cloud-client-users/src/main/java/com/study/cloudclientusers/CloudClientUsersApplication.java
@@ -0,0 +1,14 @@
+package com.study.cloudclientusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClientUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientUsersApplication.class, args);
+    }
+    // http://localhost:8082/message1
+    // http://localhost:8082/message2
+}
---
+++ cloud-router/src/test/java/com/study/cloudrouter/CloudRouterApplicationTest.java
@@ -0,0 +1,14 @@
+package com.study.cloudrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CloudRouterApplicationTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+
+}
